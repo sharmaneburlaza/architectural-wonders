a24f813b3fdb2c9592d73b315e6104fac3154bec
sharmane burlaza <sjmburlaza@gmail.com>
2023-05-20 05:54:59 +0800

    Add components routing; Install bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlphabeticalComponent } from './alphabetical/alphabetical.component';
+import { ChronologicalComponent } from './chronological/chronological.component';
+import { DetailsComponent } from './details/details.component';
+import { LocationComponent } from './location/location.component';
+import { ProgrammaticComponent } from './programmatic/programmatic.component';
+import { StyleComponent } from './style/style.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'alphabetical', component: AlphabeticalComponent },
+  { path: 'chronological', component: ChronologicalComponent },
+  { path: 'location', component: LocationComponent },
+  { path: 'programmatic', component: ProgrammaticComponent },
+  { path: 'style', component: StyleComponent },
+  { path: 'detail/:id', component: DetailsComponent },
+  { path: '', redirectTo: 'style', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { PageLayoutComponent } from './shared/page-layout/page-layout.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
